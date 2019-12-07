ce12bd6a02021ee0da1b031a0586a88fe377d981
Tim Boeckstaens <boeckstaenstim@gmail.com>
2019-12-07 12:11:31 +0100

    rewrite day 7 part 1 with coroutines

---
+++ src/main/kotlin/OpcodeDay07.kt
@@ -0,0 +1,73 @@
+class Opcode07(private val amp: String, private val data: MutableList<Int>, private val input: OpcodeInput07, private val output: OpcodeOutput07) {
+
+    suspend fun execute(): String {
+        var index = 0
+        while (true) {
+            index = executeValueOf(data, index)
+            if (data[index] == 99) break
+        }
+        println("Amp $amp done")
+        return data.joinToString(separator = ",")
+    }
+
+    private suspend fun executeValueOf(data: MutableList<Int>, index: Int): Int {
+        fun parameterValue(paramMode: Int, offset: Int): Int {
+            return if (paramMode == 0) data[index + offset] else index + offset
+        }
+        val value = data[index]
+        val operation = value % 10
+        val param1Mode = value / 100 % 10
+        val param2Mode = value / 1000 % 100
+        val param3Mode = value / 10000 % 1000
+        if (operation == 1) {
+            data[parameterValue(param3Mode, 3)] = data[parameterValue(param1Mode, 1)] + data[parameterValue(param2Mode, 2)]
+            return index + 4
+        }
+        if (operation == 2) {
+            data[parameterValue(param3Mode, 3)] = data[parameterValue(param1Mode, 1)] * data[parameterValue(param2Mode, 2)]
+            return index + 4
+        }
+        if (operation == 3) {
+            data[parameterValue(param1Mode, 1)] = input.get()
+            return index + 2
+        }
+        if (operation == 4) {
+            output.receive(data[parameterValue(param1Mode, 1)])
+            return index + 2
+        }
+        if (operation == 5) {
+            return if (data[parameterValue(param1Mode, 1)] != 0) data[parameterValue(param2Mode, 2)] else index + 3
+        }
+        if (operation == 6) {
+            return if (data[parameterValue(param1Mode, 1)] == 0) data[parameterValue(param2Mode, 2)] else index + 3
+        }
+        if (operation == 7) {
+            if (data[parameterValue(param1Mode, 1)] < data[parameterValue(param2Mode, 2)]) {
+                data[parameterValue(param3Mode, 3)] = 1
+            } else {
+                data[parameterValue(param3Mode, 3)] = 0
+            }
+            return index + 4
+        }
+        if (operation == 8) {
+            if (data[parameterValue(param1Mode, 1)] == data[parameterValue(param2Mode, 2)]) {
+                data[parameterValue(param3Mode, 3)] = 1
+            } else {
+                data[parameterValue(param3Mode, 3)] = 0
+            }
+            return index + 4
+        }
+        throw IllegalStateException("Unknown operation $operation")
+    }
+
+}
+
+interface OpcodeInput07 {
+    suspend fun get(): Int
+}
+
+
+interface OpcodeOutput07 {
+    suspend fun receive(output: Int)
+}
+
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -1,6 +1,11 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class Day07Test {
 
@@ -9,12 +14,11 @@ class Day07Test {
         val input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
         val split = splitOpcodeString(input)
 
-        val runSignal = runSignal(listOf(4, 3, 2, 1, 0), split)
-        println(runSignal)
-        //assertEquals(43210, runSignal)
+        val output = runSignal(listOf(4, 3, 2, 1, 0), split)
+        assertEquals(43210, output.value())
     }
 
-   /* @Test
+    @Test
     internal fun calculateExample1() {
         val input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
         val split = splitOpcodeString(input)
@@ -24,45 +28,50 @@ class Day07Test {
         assertEquals(listOf(4, 3, 2, 1, 0), bestSignal)
     }
 
-    @Test
-    internal fun task1() {
-        val input = Files.readAllLines(Paths.get("test/Day07"))[0]
-        val split = splitOpcodeString(input)
+     @Test
+     internal fun task1() {
+         val input = Files.readAllLines(Paths.get("src/test/resources/Day07"))[0]
+         val split = splitOpcodeString(input)
 
-        val (highestValue, bestSignal) = bestSignal(split)
-        println(highestValue)
-        println(bestSignal)
-    }*/
+         val (highestValue, bestSignal) = bestSignal(split)
+         println(highestValue)
+         println(bestSignal)
+     }
 
-   /* private fun bestSignal(split: MutableList<Int>): Pair<Int, List<Int>> {
-        val permutations = Files.readAllLines(Paths.get("test/Day07Signals")).map { it.split(" ").map { it -> Integer.parseInt(it) } }
+    private fun bestSignal(split: MutableList<Int>): Pair<Int, List<Int>> {
+        val permutations = Files.readAllLines(Paths.get("src/test/resources/Day07Signals")).map { it.split(" ").map { it -> Integer.parseInt(it) } }
 
         var highestValue = -1
         var bestSignal = listOf<Int>()
         for (signal in permutations) {
-            val inputOutput = runSignal(InputOutput(signal), split)
+            val inputOutput = runSignal(signal, split)
             if (inputOutput.value() > highestValue) {
                 bestSignal = signal
                 highestValue = inputOutput.value()
             }
         }
         return Pair(highestValue, bestSignal)
-    }*/
+    }
 
-    private fun runSignal(signal: List<Int>, split: MutableList<Int>): OpcodeOutput {
-        val ampEIO = InputOutput(mutableListOf(signal[4]))
-        val ampDIO = InputOutput(mutableListOf(signal[3]))
-        val ampCIO = InputOutput(mutableListOf(signal[2]))
-        val ampBIO = InputOutput(mutableListOf(signal[1]))
-        val ampAIO = InputOutput(mutableListOf(signal[0], 0))
+    private fun runSignal(signal: List<Int>, split: MutableList<Int>): ResultOutput {
+        val resultIO = ResultOutput()
+        val ampEIO = InputOutput("E", mutableListOf(signal[4]))
+        val ampDIO = InputOutput("D", mutableListOf(signal[3]))
+        val ampCIO = InputOutput("C", mutableListOf(signal[2]))
+        val ampBIO = InputOutput("B", mutableListOf(signal[1]))
+        val ampAIO = InputOutput("A", mutableListOf(signal[0], 0))
 
-        val ampE = Opcode(split.toMutableList(), ampEIO, ampEIO)
-        val ampD = Opcode(split.toMutableList(), ampDIO, ampEIO)
-        val ampC = Opcode(split.toMutableList(), ampCIO, ampDIO)
-        val ampB = Opcode(split.toMutableList(), ampBIO, ampCIO)
-        val ampA = Opcode(split.toMutableList(), ampAIO, ampBIO)
+        val ampE = Opcode07("E", split.toMutableList(), ampEIO, resultIO)
+        val ampD = Opcode07("D", split.toMutableList(), ampDIO, ampEIO)
+        val ampC = Opcode07("C", split.toMutableList(), ampCIO, ampDIO)
+        val ampB = Opcode07("B", split.toMutableList(), ampBIO, ampCIO)
+        val ampA = Opcode07("A", split.toMutableList(), ampAIO, ampBIO)
 
-        return ampEIO
+        runBlocking {
+            val amps = listOf(ampA, ampB, ampC, ampD, ampE).map { amp -> async { amp.execute() } }
+            amps.awaitAll()
+        }
+        return resultIO
     }
 
 //    @Test
@@ -85,23 +94,35 @@ class Day07Test {
 //        assertEquals(139629729, inputOutput.value())
 //    }
 
-    class InputOutput(private val inputs: MutableList<Int>) : OpcodeInput, OpcodeOutput {
+    class ResultOutput : OpcodeOutput07 {
+        private var result: Int = -1
+        override suspend fun receive(output: Int) {
+            result = output
+        }
 
-        //private val channel: kotlinx.coroutines.channels.Channel<Int>
-        override fun get(): Int {
-            if(inputs.isEmpty()) {
+        fun value(): Int = result
+    }
 
+    class InputOutput(private val amp: String, private val inputs: MutableList<Int>) : OpcodeInput07, OpcodeOutput07 {
+
+        private val channel: Channel<Int> = Channel()
+
+        override suspend fun get(): Int {
+            val res = if (inputs.isEmpty()) {
+                channel.receive();
+            } else {
+                val i = inputs[0]
+                inputs.removeAt(0)
+                i
             }
-            val ret = inputs[0]
-            inputs.removeAt(0)
-            return ret;
+            println("Getting from $amp $res")
+            return res;
         }
 
-        override fun receive(output: Int) {
-            inputs.add(output)
+        override suspend fun receive(output: Int) {
+            println("Receiving on $amp $output")
+            channel.send(output)
         }
-
-        fun value(): Int = inputs[0]
     }
 
 }
