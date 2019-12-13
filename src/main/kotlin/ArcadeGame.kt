ea384814327a5abc0a593ab55cb7a4378cc94a7c
Tim Boeckstaens <boeckstaenstim@gmail.com>
2019-12-13 09:06:03 +0100

    Day 13 part 2

--- src/main/kotlin/ArcadeGame.kt
+++ src/main/kotlin/ArcadeGame.kt
@@ -6,27 +6,64 @@ class ArcadeGame(
         private val board: MutableMap<ArcadeGameCoordinate, Tile>
 ) : IntcodeInput, IntcodeOutput {
     private val brain: Intcode = Intcode("ArcadeGame", memory, this, this)
+
     private var instruction = LEFT
     private var left = 0L
     private var top = 0L
+    private var score = 0L
 
-    fun run() {
+    private var paddle = ArcadeGameCoordinate(0L, 0L)
+    private var ball = ArcadeGameCoordinate(0L, 0L)
+
+    fun run(): Long {
         runBlocking { brain.execute() }
+        print()
+        return score;
     }
 
     override suspend fun get(): Long {
-        return -1
+        print()
+        if(paddle.left > ball.left) return -1
+        if(paddle.left < ball.left) return 1
+        return 0
     }
 
     override suspend fun receive(output: Long) {
         when (instruction) {
             LEFT -> left = output
             TOP -> top = output
-            TYPE -> board[ArcadeGameCoordinate(left, top)] = Tile.parse(output)
+            TYPE -> {
+                if (left == -1L && top == 0L) {
+                    score = output
+                } else {
+                    val tile = Tile.parse(output)
+                    val arcadeGameCoordinate = ArcadeGameCoordinate(left, top)
+                    if (tile == Tile.PADDLE) paddle = arcadeGameCoordinate
+                    else if (tile == Tile.BALL) ball = arcadeGameCoordinate
+                    board[arcadeGameCoordinate] = tile
+                }
+            }
         }
         instruction = instruction.next()
     }
 
+    fun print() {
+        println(score)
+        val coordinates = board.keys
+        val maxLeft = coordinates.map { coordinate -> coordinate.left }.max()!!
+        val maxTop = coordinates.map { coordinate -> coordinate.top }.max()!!
+        val lines = mutableListOf<String>()
+        for (top in 0 until maxTop) {
+            val rowArray = Array(maxLeft.toInt() + 1) { left ->
+                (board[ArcadeGameCoordinate(left.toLong(), top)] ?: Tile.EMPTY).char
+            }
+            lines.add(rowArray.joinToString(separator = ""))
+        }
+        for (line in lines) {
+            println(line)
+        }
+        println()
+    }
 }
 
 data class ArcadeGameCoordinate(val left: Long, val top: Long)
