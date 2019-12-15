01e7195938cb982ee1e07fa5510a5a69e9443d92
Tim Boeckstaens <boeckstaenstim@gmail.com>
2019-12-15 14:56:52 +0100

    day 14 part 1

--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class Day14Test {
 
@@ -13,19 +16,95 @@ class Day14Test {
                         "7 A, 1 E => 1 FUEL"
                 ).split("\n")
         val reactions = input.map { parseReaction(it) }
-
         val reactionsMap = reactions.map { it.produces.chemical to it }.toMap()
 
-        val produce = produce(Chemical("FUEL"), reactionsMap)
-        println(produce)
+        val cost = produce(Chemical("FUEL"), reactionsMap, mutableMapOf())
+        assertEquals(31, cost)
     }
 
-    private fun produce(toProduce: Chemical, reactions: Map<Chemical, Reaction>): Int {
+    @Test
+    internal fun simple2() {
+        val input = (
+                "9 ORE => 2 A\n" +
+                        "8 ORE => 3 B\n" +
+                        "7 ORE => 5 C\n" +
+                        "3 A, 4 B => 1 AB\n" +
+                        "5 B, 7 C => 1 BC\n" +
+                        "4 C, 1 A => 1 CA\n" +
+                        "2 AB, 3 BC, 4 CA => 1 FUEL"
+                ).split("\n")
+        val reactions = input.map { parseReaction(it) }
+        val reactionsMap = reactions.map { it.produces.chemical to it }.toMap()
+
+        val cost = produce(Chemical("FUEL"), reactionsMap, mutableMapOf())
+        assertEquals(165, cost)
+    }
+
+    @Test
+    internal fun complex() {
+        val input = (
+                "171 ORE => 8 CNZTR\n" +
+                        "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n" +
+                        "114 ORE => 4 BHXH\n" +
+                        "14 VRPVC => 6 BMBT\n" +
+                        "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n" +
+                        "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n" +
+                        "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n" +
+                        "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n" +
+                        "5 BMBT => 4 WPTQ\n" +
+                        "189 ORE => 9 KTJDG\n" +
+                        "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n" +
+                        "12 VRPVC, 27 CNZTR => 2 XDBXC\n" +
+                        "15 KTJDG, 12 BHXH => 5 XCVML\n" +
+                        "3 BHXH, 2 VRPVC => 7 MZWV\n" +
+                        "121 ORE => 7 VRPVC\n" +
+                        "7 XCVML => 6 RJRHP\n" +
+                        "5 BHXH, 4 VRPVC => 5 LTCX"
+                ).split("\n")
+        val reactions = input.map { parseReaction(it) }
+        val reactionsMap = reactions.map { it.produces.chemical to it }.toMap()
+
+        val cost = produce(Chemical("FUEL"), reactionsMap, mutableMapOf())
+        assertEquals(2210736, cost)
+    }
+
+    @Test
+    internal fun task1() {
+        val input = Files.readAllLines(Paths.get("src/test/resources/Day14"))
+        val reactions = input.map { parseReaction(it) }
+        val reactionsMap = reactions.map { it.produces.chemical to it }.toMap()
+
+        val cost = produce(Chemical("FUEL"), reactionsMap, mutableMapOf())
+        println(cost)
+    }
+
+    private fun produce(toProduce: Chemical, reactions: Map<Chemical, Reaction>, stock: MutableMap<Chemical, Int>): Int {
         val reaction = reactions[toProduce] ?: error("No reaction for $toProduce?")
-        if(reaction.reagents.size == 1 && reaction.reagents[0].chemical == Chemical("ORE")) {
-            return reaction.reagents[0].amount
+        return if (reaction.reagents.size == 1 && reaction.reagents[0].chemical == Chemical("ORE")) {
+            stock[toProduce] = (stock[toProduce] ?: 0) + reaction.produces.amount
+            //println("Added ${reaction.produces.amount} of $toProduce")
+            reaction.reagents[0].amount
+        } else {
+            //println(reaction)
+            val sum = reaction.reagents.map { reagent -> get(reagent.chemical, reagent.amount, reactions, stock) }.sum()
+            stock[toProduce] = (stock[toProduce] ?: 0) + reaction.produces.amount
+            //println("Added ${reaction.produces.amount} of $toProduce")
+            sum
         }
-        return reaction.reagents.map { reagent -> produce(reagent.chemical, reactions) }.sum()
+    }
+
+    private fun get(toGet: Chemical, amount: Int, reactions: Map<Chemical, Reaction>, stock: MutableMap<Chemical, Int>): Int {
+        //println("Getting $amount $toGet stock: $stock")
+        val amountInStock = stock[toGet] ?: 0
+        return if (amountInStock >= amount) {
+            //println("Enough $toGet in stock")
+            stock[toGet] = amountInStock - amount
+            0
+        } else {
+            //println("Not enough $toGet in stock, producing")
+            val oreCost = produce(toGet, reactions, stock)
+            oreCost + get(toGet, amount, reactions, stock)
+        }
     }
 
     private fun parseReaction(reaction: String): Reaction {
